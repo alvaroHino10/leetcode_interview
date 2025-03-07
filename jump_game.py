24caeaf963bf1ead385e584b84138379873e1c42
Alvaro Hinojosa Espinoza <alvaro_hino@hotmail.com>
2025-03-07 12:24:50 -0400

    Modificacion a la documentacion

--- jump_game.py
+++ jump_game.py
@@ -4,13 +4,13 @@ from typing import List
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         """
-            Se verifica si se puede llegar al final de la lista de números a partir de la posición 0. Se recorre la
-            lista desde el final hacia el inicio, se verifica si la suma de la posición actual y el valor en esa
-            posición es mayor o igual a la meta, si es así se actualiza la meta a la posición actual. Al final se
-            verifica si la meta es igual a 0.
+            Se verifica si se puede llegar al final de la lista de números a partir de la posición 0.
         :param nums: List[int] Lista de numeros de salto
         :return: bool: True si se puede llegar al final, False en caso contrario
         """
+        # Se recorre la lista desde el final hacia el inicio, se verifica si la suma de la posición actual y el valor en
+        # esa posición es mayor o igual a la meta, si es así se actualiza la meta a la posición actual. Al final se
+        # verifica si la meta es igual a 0.
         goal = len(nums) - 1
         for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= goal:
